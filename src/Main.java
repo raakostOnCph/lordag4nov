044cef744f804ab8a9feb5c9a003e2c8e986790b
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2023-11-08 09:53:57 +0100

    til emiel

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main
 {
@@ -9,23 +6,48 @@ public class Main
     {
 
 
+//
+//
+//        Bank bank = new Bank();
+//
+//        bank.opretKonto("patrick", "password");
+//
+//        bank.logOn("patrick", "password");
+//
+//        System.out.println(bank.getAktivKonto().navn);
+//
+//        System.out.println(bank.logOn("simone", "jegtagerendramkl.otte 00"));
+//
+//        try {
+//            System.out.println(bank.getAktivKonto().navn);
+//        } catch (Exception e) {
+//            System.out.println("du er ikke logget ind");
+//        }
+
+        Map<String, Integer> integerMap1 = new HashMap<>();
+        Map<String, Integer> integerMap2 = new HashMap<>();
+
+        integerMap1.put("nik1", 1);
+        integerMap1.put("nik2", 2);
+        integerMap1.put("nik3", 3);
+        integerMap1.put("nik4", 4);
+        integerMap1.put("nik", 8);
 
 
-        Bank bank = new Bank();
+        Collection<Integer> collection = integerMap1.values();
 
-        bank.opretKonto("patrick", "password");
 
-        bank.logOn("patrick", "password");
 
-        System.out.println(bank.getAktivKonto().navn);
 
-        System.out.println(bank.logOn("simone", "jegtagerendramkl.otte 00"));
+        ArrayList <Integer> integers = new ArrayList<Integer>((integerMap1.values()) );
 
-        try {
-            System.out.println(bank.getAktivKonto().navn);
-        } catch (Exception e) {
-            System.out.println("du er ikke logget ind");
+        for (Integer integer : integers) {
+            System.out.println(integer);
         }
+
+            ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList({1,2,3,4,5}))
+
+
 
 
     }
